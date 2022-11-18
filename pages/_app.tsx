47f60c9ab6f19c7fb5a7ece2115a2ea1ef3dfaa7
이주연 aspyn <dldbsrud0925@gmail.com>
2022-11-18 16:40:35 +0900

    css setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import theme from '../styles/theme';
+import GlobalStyles from '../styles/GlobalStyles';
+
+import 'styles/index.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </>
+  );
 }
---
+++ styles/theme/index.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    white: '#FFF',
+    black: '#000',
+    red: '#F00',
+    brown: '#a08679',
+    gray99: '#999999',
+    grayEE: '#eeeeee',
+    gray66: '#666666',
+  },
+};
+
+export type Theme = typeof theme;
+
+export default theme;
