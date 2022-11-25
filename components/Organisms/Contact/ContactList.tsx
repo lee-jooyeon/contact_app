5e8f6350cd108fba36ce79ab0b19fdf3bfeaf475
이주연 aspyn <dldbsrud0925@gmail.com>
2022-11-25 10:55:05 +0900

    main 브랜치로 바뀌었나 test

--- components/Organisms/Contact/ContactList.tsx
+++ components/Organisms/Contact/ContactList.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { Box } from '../../Atoms';
 import ContactItem from './ContactItem';
 
+// 자식 컴포넌트에 넘겨주기 위해 export 정의
 export type dataType = {
   id: number;
   name: string;
@@ -12,6 +13,7 @@ export type dataType = {
 };
 
 export default function ContactList() {
+  // lists Array state에 제네릭 타입을 지정해 줍니다.
   const [lists, setLists] = useState<dataType[]>([]);
 
   useEffect(() => {
